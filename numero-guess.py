35dce51c206e17fe1f74475a6d52c6f335ebc3db
StyflerRick <108999162+StyflerRick@users.noreply.github.com>
2023-03-06 17:32:41 -0300

    Adivinhação

--- numero-guess.py
+++ numero-guess.py
@@ -1,24 +1,23 @@
 import random
 
 tentativas = [1]
-randomnum = random.randint(0, 100)  # Numero aleatorio de 0 a 100
+randomnum = random.randint(0, 10)  # Numero aleatorio de 0 a 100
 randomnum = int(randomnum)  # Define variavel como 'int'
-print('Numero aleatorio é {}'.format(randomnum))
 
 while True:
     try:
-        tentativa = int(input('Chute um numero de 0 a 100: '))
-        if tentativa == randomnum:
+        chute = int(input('Chute um numero de 0 a 10: '))
+        if chute == randomnum:
             print('Você acertou o numero.')
-            print(f'O numero aleatorio era {randomnum}.')
+            print('O Numero de tentativas foi', sum(tentativas))
             break
-        elif tentativa > 100:
-            print('Somente numeros entre 0 e 100.')
+        elif chute > 10:
+            print('Somente numeros entre 0 e 10.')
             tentativas.append(1)
+            print(f'{sum(tentativas)}ª tentativa')
         else:
             print('Numero errado.')
             tentativas.append(1)
+            print(f'{sum(tentativas)}ª tentativa')
     except ValueError:
         print('ValueError: Digite apenas numeros inteiros.')
-
-print('O Numero de tentativas foi', sum(tentativas))
